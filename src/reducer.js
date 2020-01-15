50e1276dd7755f3da2b3739915f97b071a311fd4
Scott Dodge <scott@goldilocks.design>
2020-01-15 14:34:56 -0600

    move reducer to separate file

---
+++ src/reducer.js
@@ -0,0 +1,76 @@
+import { beverages } from './data'
+
+export function reducer(state, action) {
+  switch (action.type) {
+    case 'setDose':
+      let dose = action.payload || newDose()
+      return {
+        ...state, 
+        ...dose
+      }
+    case 'setDrink':
+      return {
+        ...state, 
+        drink: action.payload,
+        quantity: action.payload.oz
+      }
+    case 'setQuantity':
+      return {
+        ...state, 
+        quantity: action.payload
+      }
+    case 'setHour':
+      return {
+        ...state, 
+        hour: action.payload
+      }
+    case 'setId':
+      return {
+        ...state, 
+        id: action.payload
+      }
+    case 'saveDose':
+      return {
+        ...state,
+        drinks: [
+          ...state.drinks.filter(dose => state.id !== dose.id), 
+          {
+            id: state.id,
+            drink: state.drink,
+            quantity: state.quantity,
+            hour: state.hour
+          }
+        ]
+      }
+    case 'removeDose':
+      return {
+        ...state,
+        drinks: state.drinks.filter(dose => action.payload !== dose.id.toString())
+      }
+    case 'setIsAddEdit':
+        return {
+          ...state, 
+          isAddEdit: action.payload
+        }
+    default:
+      throw new Error()
+  }
+}
+
+const newDose = () => ({
+  id: new Date().getTime(),
+  drink: beverages[0],
+  quantity: beverages[0].oz,
+  hour: 8
+})
+
+export const initialState = {
+  id: null,
+  drinks: [],
+  drink: beverages[0],
+  quantity: beverages[0].oz,
+  hour: 8,
+  isAddEdit: false
+}
+
+export default { reducer, initialState }
